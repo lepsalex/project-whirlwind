f04a2a8a840c701b624fbf45b60d9bf54ad61b2e
Alex Lepsa <lepsalex@gmail.com>
2026-03-07 18:26:33 -0500

    added raylib and some test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
+#include <raylib.h>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello\n";
+	SetTargetFPS(60);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "Project Whirlwind");
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText("Hello World!", 320, 200, 20, RED);
+		DrawRectangle(10, 10, 200,200, {120, 100, 0, 100});
+		DrawRectangle(30, 30, 100,100, {120, 100, 10, 127});
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }
---
+++ third-party/raylib-5.5/examples/audio/resources/mini1111.xm
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70469f68456efe7bab471ade2563d9913d45b8f931e33313c623d4271c9e218a
+size 25676
