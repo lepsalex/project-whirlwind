9f05acaf5cc73f90ea95d53ce1ab5dd5d81dbb94
Alex Lepsa <lepsalex@gmail.com>
2026-03-07 19:09:07 -0500

    add imgui and rlimgui backend with some test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,40 @@
 #include <raylib.h>
 #include <iostream>
 
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main()
 {
 	SetTargetFPS(60);
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "Project Whirlwind");
 
+	rlImGuiSetup(true);
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+
+		rlImGuiBegin();
 
 		DrawText("Hello World!", 320, 200, 20, RED);
 		DrawRectangle(10, 10, 200,200, {120, 100, 0, 100});
 		DrawRectangle(30, 30, 100,100, {120, 100, 10, 127});
 
+		ImGui::Begin("test");
+
+		ImGui::Text("hello");
+		ImGui::Button("button");
+
+		ImGui::End();
+
+		rlImGuiEnd();
+
 		EndDrawing();
 	}
+
+	rlImGuiShutdown();
 
 	CloseWindow();
 
